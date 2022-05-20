381a8e0ba72902569b4ae4b299f9022cb187963a
Sam Felice <samwfelice@gmail.com>
2022-05-20 19:54:45 +0530

    Introduce Buggy

---
+++ app.js
@@ -0,0 +1,6 @@
+const name = "Samuel Felice";
+//loop to console.log each alphabet of the variable name
+for (let elements of name){
+  console.log(name);
+}
+//this code should console.log each alphabet in its own line of the variable name
